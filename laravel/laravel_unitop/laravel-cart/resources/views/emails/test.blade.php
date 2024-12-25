9e3e2d3aa19ac52f41a55c8b4c5f6f4b35e6442d
dangquocthanh <dangquocthanh2812@gmail.com>
2024-12-25 14:37:53 +0700

    22

---
+++ laravel/laravel_unitop/laravel-cart/resources/views/emails/test.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Send Email</title>
+</head>
+
+<body>
+    <h1>Hello, {{ $name }}!</h1>
+    <p>This email was sent from Laravel using SMTP with Gmail.</p>
+</body>
+
+</html>
